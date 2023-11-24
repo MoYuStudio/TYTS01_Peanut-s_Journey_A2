5f8113d2d4732bd93cbe24b9c97ef591baf0dab0
WilsonVinson <cprewilsonvinson@gmail.com>
2023-11-24 14:39:20 +0800

    main

--- project/utility/global.gd
+++ project/utility/global.gd
@@ -8,4 +8,4 @@ func _ready():
 	Engine.set_max_fps(60)
 	DisplayServer.window_set_size(Vector2(1280,720))
 	DisplayServer.window_set_position(Vector2(100,100))
-	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	# DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
