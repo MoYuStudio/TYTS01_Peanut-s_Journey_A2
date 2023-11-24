dab7bc1d0b7be772269c972f8b636ace8b8a115f
WilsonVinson <cprewilsonvinson@gmail.com>
2023-11-24 11:22:03 +0800

    main

--- project/resources/character.gd
+++ project/resources/character.gd
@@ -5,6 +5,7 @@ class_name Character
 
 @export var display_name := "Player"
 
-@export var run_speed := 600.0
-
 @export var level := 1
+
+@export var money := 1
+@export var hp := 1
--- project/utility/global.gd
+++ project/utility/global.gd
@@ -1,2 +1,11 @@
 
 extends Node
+
+func _ready():
+
+	DisplayServer.window_set_title("花生迷途 Peanut's Journey")
+	
+	Engine.set_max_fps(60)
+	DisplayServer.window_set_size(Vector2(1280,720))
+	DisplayServer.window_set_position(Vector2(100,100))
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
