9cae1e1b4e75fa7716bc822f5cedc6fe19ee5222
WilsonVinson <cprewilsonvinson@gmail.com>
2023-12-14 16:27:51 +0800

    架构更新

--- project/resources/item_data.gd
+++
@@ -1,9 +0,0 @@
-
-extends Resource
-
-class_name ItemData
-
-@export var unique_id := ""
-@export var display_name := ""
-@export var description := ""
-@export var icon: Texture2D
---
+++ project/scripts/item_data.gd
@@ -0,0 +1,9 @@
+
+extends Resource
+
+class_name ItemData
+
+@export var unique_id := ""
+@export var display_name := ""
+@export var description := ""
+@export var icon: Texture2D
